e96cf66cbba5e1631b9fabd716ff6b4c919bc00b
Michael <suwyn45@gmail.com>
2018-02-22 15:21:13 -0700

    trialcommit

---
+++ Hangman/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main()
+        {
+            string guy = "\\O/\n |\n /\\";
+            string start = " ";
+            words word = new words();
+            string MyWord = word.deal();
+            int MyWordnum = MyWord.Length;
+            string Hidden = MyWordnum.ToString();
+            bool IsGameOver = false;
+            int Strikes = 0;
+            
+            
+            
+
+
+
+
+
+
+            Console.WriteLine("Type go to start!");
+
+            start = Console.ReadLine();
+            if (start == "go")
+            {
+                StringBuilder exes = new StringBuilder("");
+                for(int i = 0; i<MyWordnum; i++)
+                {
+
+                    exes.Append("#");
+                }
+                
+                
+                Console.WriteLine(Hidden + "letters to be guessed");
+                
+
+                while (!IsGameOver)
+                {
+                    Console.WriteLine(exes.ToString());
+                    Console.WriteLine("Guess a letter");
+                    char guess = Console.ReadLine()[0];
+                    bool isGuessCorrect = false;
+                    for (int i = 0; i < MyWord.Length; i++)
+                    {
+
+                        if (guess == MyWord[i])
+                        {
+
+                            exes[i] = guess;
+                            isGuessCorrect = true;
+                            
+                        }                
+                       
+                        
+                    }
+
+                    if (!isGuessCorrect)
+                    {
+                        Strikes++;
+                    }
+                    if(Strikes == 10)
+                    {
+                        IsGameOver = true;
+                        Console.WriteLine("You Lose!  The Word Was " + MyWord);
+                        Console.WriteLine(guy);
+                    }
+                    if(exes.ToString() == MyWord)
+                    {
+                        IsGameOver = true;
+                        Console.WriteLine(MyWord);
+                        Console.WriteLine("You Win!");
+                    }
+                        
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("oops");
+            }
+
+
+         
+
+           
+         
+            
+           
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
---
+++ Hangman/words.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman
+{
+    class words
+    {
+        List<string> OptionList = new List<string>()
+        { "watermelon", "apple", "cucumber", "doll", "football", "soccer", "couch", "carrot", "animal", "elephant", "computer", "hiking", "jeans", "shirt", "telephone",
+        "wallet", "church", "candy", "water", "pizza", "hotdog", "firework", "baseball", "table", "spoon", "flipflops", "kangaroo", "america", "australia", "germany", "fishing",
+        "musket", "rifle", "samurai", "boxing", "backpack", "escalator", "elevator", "shopping", "beautiful", "dolphin", "rainbow", "president", "shotgun", "forest", "desert",
+        "whiskey", "picture", "pillow", "blanket", "guitar", "piano", "tree", "teacup", "boiling", "stovetop", "oven", "coconut", "pineapple", "airplane", "firetruck"};
+
+
+       public string deal()
+
+        {
+            Shuffle(OptionList);
+            string NumOne = OptionList[0];
+            OptionList.RemoveAt(0);
+            return NumOne;
+            
+        }
+       
+            public static void Shuffle(List<string> list)
+            {
+                int n = list.Count;
+                Random rnd = new Random();
+                while (n > 1)
+                {
+                    int k = (rnd.Next(0, n) % n);
+                    n--;
+                    string value = list[k];
+                    list[k] = list[n];
+                    list[n] = value;
+                }
+            } 
+
+        }
+
+
+    }
+    
+
+    
+
+
+
